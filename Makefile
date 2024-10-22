79dc8edbbfa13b6225162aa5ab72116955a0d4a1
csjh <61282104+csjh@users.noreply.github.com>
2024-10-22 00:32:23 -0400

    add test execution file

--- Makefile
+++ Makefile
@@ -4,7 +4,8 @@ CFLAGS := -Wall -Wextra -std=c++20
 
 debug: CFLAGS += -g3 -DWASM_DEBUG -fsanitize=address
 release: CFLAGS += -O2
-debug: executable
+
+debug: executable tests
 
 interpreter.o: interpreter.cpp interpreter.hpp
 	clang++ interpreter.cpp -c $(CFLAGS)
@@ -15,9 +16,13 @@ validator.o: validator.cpp validator.hpp
 main.o: main.cpp
 	clang++ main.cpp -c $(CFLAGS)
 
+tests: interpreter.o validator.o ./test/executor.cpp
+	clang++ test/executor.cpp interpreter.o validator.o -o test/executor $(CFLAGS)
+
 executable: interpreter.o validator.o main.o
 	clang++ interpreter.o validator.o main.o -o main $(CFLAGS)
 
 clean:
 	rm -f *.o
 	rm main
+	rm test/executor
--- test/executor.cpp
+++ test/executor.cpp
@@ -1,7 +1,12 @@
+#include "../interpreter.hpp"
+#include "../validator.hpp"
 #include "json.hpp"
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <system_error>
+#include <variant>
 #include <vector>
 
 struct value {
@@ -10,6 +15,25 @@ struct value {
 };
 
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(value, type, value)
+
+std::vector<Mitey::WasmValue> to_wasm_values(const std::vector<value> &values) {
+    std::vector<Mitey::WasmValue> result;
+    for (auto &v : values) {
+        if (v.type == "i32") {
+            result.push_back(
+                Mitey::WasmValue(static_cast<uint32_t>(std::stoul(v.value))));
+        } else if (v.type == "i64") {
+            result.push_back(Mitey::WasmValue(std::stoull(v.value)));
+        } else if (v.type == "f32") {
+            result.push_back(Mitey::WasmValue(std::stof(v.value)));
+        } else if (v.type == "f64") {
+            result.push_back(Mitey::WasmValue(std::stod(v.value)));
+        } else {
+            std::cerr << "Unknown type: " << v.type << std::endl;
+        }
+    }
+    return result;
+}
 
 struct only_type {
     std::string type;
@@ -91,7 +115,8 @@ using Tests = std::variant<test_module, test_malformed, test_return,
 
 namespace nlohmann {
 template <> struct adl_serializer<Tests> {
-    static void to_json(json &j, const Tests &opt) { /* not implemented */
+    static void to_json(json &j, const Tests &opt) {
+        std::visit([&j](auto &&arg) { j = arg; }, opt);
     }
 
     static void from_json(const json &j, Tests &opt) {
@@ -121,6 +146,31 @@ struct wastjson {
 
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(wastjson, source_filename, commands)
 
+Mitey::Instance from_file(const std::string &filename) {
+    std::ifstream wasm_file{filename, std::ios::binary};
+    if (!wasm_file) {
+        throw std::system_error(errno, std::system_category(), filename);
+    }
+
+    wasm_file.seekg(0, std::ios::end);
+    long length = wasm_file.tellg();
+    wasm_file.seekg(0, std::ios::beg);
+
+    // load data into a unique_ptr
+    std::unique_ptr<uint8_t, void (*)(uint8_t *)> bytes(
+        new uint8_t[length], [](uint8_t *ptr) { delete[] ptr; });
+
+    wasm_file.read(reinterpret_cast<char *>(bytes.get()), length);
+    wasm_file.close();
+
+    return Mitey::Instance(std::move(bytes), length);
+}
+
+namespace fs = std::filesystem;
+fs::path resolve_relative(const fs::path &file1, const fs::path &file2) {
+    return fs::absolute(file1.parent_path() / file2).lexically_normal();
+}
+
 int main(int argv, char **argc) {
     if (argv != 2) {
         std::cerr << "Usage: " << argc[0] << " <filename>" << std::endl;
@@ -137,68 +187,94 @@ int main(int argv, char **argc) {
 
     auto wast = j.template get<wastjson>();
 
-    std::cout << "source_filename: " << wast.source_filename << std::endl;
+    std::cout << "Testing file: " << wast.source_filename << std::endl;
+    if (!std::holds_alternative<test_module>(wast.commands[0])) {
+        std::cerr << "First command is not a module" << std::endl;
+        return 1;
+    }
+    test_module &m = std::get<test_module>(wast.commands[0]);
+    Mitey::Instance instance =
+        from_file(resolve_relative(filename, m.filename));
+    wast.commands.erase(wast.commands.begin());
+
     for (auto &t : wast.commands) {
+        nlohmann::json j = t;
+        std::cerr << "Running test: " << j << std::endl;
+
         if (std::holds_alternative<test_module>(t)) {
-            auto &m = std::get<test_module>(t);
-            std::cout << "type: " << m.type << std::endl;
-            std::cout << "line: " << m.line << std::endl;
-            std::cout << "filename: " << m.filename << std::endl;
+            std::cerr << "Duplicate module found" << std::endl;
+            return 1;
         } else if (std::holds_alternative<test_malformed>(t)) {
-            auto &m = std::get<test_malformed>(t);
-            std::cout << "type: " << m.type << std::endl;
-            std::cout << "line: " << m.line << std::endl;
-            std::cout << "filename: " << m.filename << std::endl;
-            std::cout << "text: " << m.text << std::endl;
-            std::cout << "module_type: " << m.module_type << std::endl;
+            // auto &m = std::get<test_malformed>(t);
+            // malformation is only for wat
+            continue;
         } else if (std::holds_alternative<test_return>(t)) {
             auto &m = std::get<test_return>(t);
-            std::cout << "type: " << m.type << std::endl;
-            std::cout << "line: " << m.line << std::endl;
-            std::cout << "action.type: " << m.action.type << std::endl;
-            std::cout << "action.field: " << m.action.field << std::endl;
-            for (auto &a : m.action.args) {
-                std::cout << "args.type: " << a.type << std::endl;
-                std::cout << "args.value: " << a.value << std::endl;
+
+            auto result =
+                instance.execute(m.action.field, to_wasm_values(m.action.args));
+
+            if (result.size() != m.expected.size()) {
+                std::cerr << "Expected " << m.expected.size()
+                          << " results but got " << result.size() << std::endl;
             }
-            for (auto &a : m.expected) {
-                std::cout << "expected.type: " << a.type << std::endl;
-                std::cout << "expected.value: " << a.value << std::endl;
+
+            auto expected_results = to_wasm_values(m.expected);
+
+            auto test = [](std::string &ty, Mitey::WasmValue &res,
+                           Mitey::WasmValue &exp) {
+                if (ty == "i32" && res.i32 != exp.i32) {
+                    std::cerr << "Expected: " << exp.i32
+                              << " but got: " << res.i32 << std::endl;
+                    return false;
+                } else if (ty == "i64" && res.i64 != exp.i64) {
+                    std::cerr << "Expected: " << exp.i64
+                              << " but got: " << res.i64 << std::endl;
+                    return false;
+                } else if (ty == "f32" && res.f32 != exp.f32) {
+                    std::cerr << "Expected: " << exp.f32
+                              << " but got: " << res.f32 << std::endl;
+                    return false;
+                } else if (ty == "f64" && res.f64 != exp.f64) {
+                    std::cerr << "Expected: " << exp.f64
+                              << " but got: " << res.f64 << std::endl;
+                    return false;
+                }
+                return true;
+            };
+
+            for (uint32_t i = 0; i < result.size(); ++i) {
+                if (!test(m.expected[i].type, result[i], expected_results[i])) {
+                    return 1;
+                }
             }
         } else if (std::holds_alternative<test_invalid>(t)) {
             auto &m = std::get<test_invalid>(t);
-            std::cout << "type: " << m.type << std::endl;
-            std::cout << "line: " << m.line << std::endl;
-            std::cout << "filename: " << m.filename << std::endl;
-            std::cout << "text: " << m.text << std::endl;
-            std::cout << "module_type: " << m.module_type << std::endl;
+            try {
+                Mitey::Instance invalid_instance =
+                    from_file(resolve_relative(filename, m.filename));
+
+                std::cerr << "Expected validation error for file: "
+                          << m.filename << std::endl;
+            } catch (Mitey::validation_error &e) {
+            }
         } else if (std::holds_alternative<test_trap>(t)) {
             auto &m = std::get<test_trap>(t);
-            std::cout << "type: " << m.type << std::endl;
-            std::cout << "line: " << m.line << std::endl;
-            std::cout << "action.type: " << m.action.type << std::endl;
-            std::cout << "action.field: " << m.action.field << std::endl;
-            for (auto &a : m.action.args) {
-                std::cout << "args.type: " << a.type << std::endl;
-                std::cout << "args.value: " << a.value << std::endl;
-            }
-            for (auto &a : m.expected) {
-                std::cout << "expected.type: " << a.type << std::endl;
+            try {
+                auto result = instance.execute(m.action.field,
+                                               to_wasm_values(m.action.args));
+
+                std::cerr << "Expected trap for action: " << m.action.field
+                          << std::endl;
+            } catch (Mitey::trap_error &e) {
+                if (e.what() != m.text) {
+                    std::cerr << "Expected: " << m.text
+                              << " but got: " << e.what() << std::endl;
+                }
             }
         } else if (std::holds_alternative<test_exhaustion>(t)) {
-            auto &m = std::get<test_exhaustion>(t);
-            std::cout << "type: " << m.type << std::endl;
-            std::cout << "line: " << m.line << std::endl;
-            std::cout << "action.type: " << m.action.type << std::endl;
-            std::cout << "action.field: " << m.action.field << std::endl;
-            for (auto &a : m.action.args) {
-                std::cout << "args.type: " << a.type << std::endl;
-                std::cout << "args.value: " << a.value << std::endl;
-            }
-            for (auto &a : m.expected) {
-                std::cout << "expected.type: " << a.type << std::endl;
-                std::cout << "expected.value: " << a.value << std::endl;
-            }
+            // auto &m = std::get<test_exhaustion>(t);
+            // todo: see if there's some memory trick to trap efficiently
         }
     }
 }
